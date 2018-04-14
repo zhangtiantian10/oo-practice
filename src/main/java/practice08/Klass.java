d2d214b17f2d3024152635e73fd97bcfa247945c
zhang tian <975571814@qq.com>
2018-04-15 01:06:02 +0800

    write practice08
    Signed-off-by: zhang tian <975571814@qq.com>

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,30 @@
 package practice08;
 
 public class Klass {
+  private Integer number;
+  private Student leader;
+
+  public Klass(Integer number) {
+    this.number = number;
+  }
+
+  public Integer getNumber() {
+    return number;
+  }
+
+  public void setNumber(Integer number) {
+    this.number = number;
+  }
+
+  public void assignLeader(Student student) {
+    this.leader = student;
+  }
+
+  public Student getLeader() {
+    return leader;
+  }
+
+  String getDisplayName() {
+    return "Class 2";
+  }
 }
