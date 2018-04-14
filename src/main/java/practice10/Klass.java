2824d7e4c7e6167a39642801b8dfeec36a3b0b61
zhang tian <975571814@qq.com>
2018-04-15 01:32:28 +0800

    write practice10
    Signed-off-by: zhang tian <975571814@qq.com>

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,57 @@
 package practice10;
 
 public class Klass {
+  private Integer number;
+  private Student leader;
+
+  public Klass(Integer number) {
+    this.number = number;
+  }
+
+  public Integer getNumber() {
+    return number;
+  }
+
+  public void setNumber(Integer number) {
+    this.number = number;
+  }
+
+  public void assignLeader(Student student) {
+    if (student.getKlass().equals(this)) {
+      this.leader = student;
+    } else {
+      System.out.println("It is not one of us.");
+    }
+  }
+
+  public Student getLeader() {
+    return leader;
+  }
+
+  String getDisplayName() {
+    return "Class " + number;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    Klass klass = (Klass) o;
+
+    return number == klass.number;
+  }
+
+  @Override
+  public int hashCode() {
+    return number;
+  }
+
+  public void appendMember(Student student) {
+    student.setKlass(this);
+  }
+
+  public boolean isIn(Student student) {
+    return student.getKlass().equals(this);
+  }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,55 @@
 package practice10;
 
-public class Teacher {
+import java.util.Set;
+
+import static java.util.stream.Collectors.joining;
+
+public class Teacher extends Person {
+
+  private Set<Klass> classes;
+
+  public Teacher(int id, String name, int age) {
+    super(id, name, age);
+  }
+
+  public Teacher(int id, String name, int age, Set<Klass> classes) {
+    super(id, name, age);
+    this.classes = classes;
+  }
+
+  public Set<Klass> getClasses() {
+    return classes;
+  }
+
+  public void setClasses(Set<Klass> classes) {
+    this.classes = classes;
+  }
+
+  String introduce() {
+    String result = new Person(this.getId(), this.getName(), this.getAge()).introduce() + " I am a Teacher.";
+
+    if (this.classes == null || this.classes.isEmpty()) {
+      return result + " I teach No Class.";
+    }
+
+    String classesStr = this.classes.stream()
+      .map(klass -> String.valueOf(klass.getNumber()))
+      .collect(joining(", "));
+
+    return result + " I teach Class " + classesStr + ".";
+  }
+
+  String introduceWith(Student student) {
+    String result = new Person(this.getId(), this.getName(), this.getAge()).introduce() + " I am a Teacher.";
+
+    if (classes.contains(student.getKlass())) {
+      return result + " I teach " + student.getName() + ".";
+    }
+
+    return result + " I don't teach " + student.getName() + ".";
+  }
+
+  public boolean isTeaching(Student student) {
+    return this.classes.stream().anyMatch(c -> c.isIn(student));
+  }
 }
