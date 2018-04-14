f9147c8a0c7314faee1bd572c37891fec0b15eb7
zhang tian <975571814@qq.com>
2018-04-15 00:11:36 +0800

    write practice05
    Signed-off-by: zhang tian <975571814@qq.com>

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,23 @@
 package practice05;
 
-public class Student {
+public class Student extends Person{
+
+  private int klass;
+
+  public Student(String name, int age, int klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  public int getKlass() {
+    return klass;
+  }
+
+  public void setKlass(int klass) {
+    this.klass = klass;
+  }
+
+  String introduce() {
+    return new Person(this.getName(), this.getAge()).introduce() + " I am a Student. I am at Class " + this.getKlass() + ".";
+  }
 }
--- src/main/java/practice05/Worker.java
+++ src/main/java/practice05/Worker.java
@@ -1,4 +1,11 @@
 package practice05;
 
-public class Worker {
+public class Worker extends Person {
+  public Worker(String name, int age) {
+    super(name, age);
+  }
+
+  String introduce() {
+    return new Person(this.getName(), this.getAge()).introduce() + " I am a Worker. I have a job.";
+  }
 }
