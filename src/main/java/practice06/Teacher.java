5f32c5dccda8988577dd275e17d4ed61d8b089c6
zhang tian <975571814@qq.com>
2018-04-15 00:24:38 +0800

    write practice06
    Signed-off-by: zhang tian <975571814@qq.com>

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,33 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+
+  private Integer klass;
+
+  public Teacher(String name, int age) {
+    super(name, age);
+  }
+
+  public Teacher(String name, int age, int klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  public Integer getKlass() {
+    return klass;
+  }
+
+  public void setKlass(int klass) {
+    this.klass = klass;
+  }
+
+  String introduce() {
+    String result = new Person(this.getName(), this.getAge()).introduce() + " I am a Teacher.";
+
+    if (this.getKlass() == null) {
+      return result + " I teach No Class.";
+    }
+
+    return result + " I teach Class 2.";
+  }
 }
